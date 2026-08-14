c3437d866dc59ce223d3ccd08e5db3315898cf01
ashutosh-das <ashutoshkumar.das@saaslabs.co>
2026-08-14 17:49:51 +0530

    Time: 103 ms (22.31%), Space: 58.4 MB (15.28%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+    let dp = new Array(nums.length).fill(1);
+    dp[nums.length - 1] = 1;
+    let max = 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        let j = i + 1;
+        while (j < nums.length) {
+            if (nums[i] < nums[j]) {
+                dp[i] = Math.max(1 + dp[j], dp[i]);;   
+            }
+            j++
+            //max = Math.max(max, dp[i])
+        }
+    }
+    console.log(dp)
+    return Math.max(...dp)
+};
