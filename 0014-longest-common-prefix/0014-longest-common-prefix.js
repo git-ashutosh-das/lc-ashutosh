17322027742c5cd12b32e09aca561291eb84765a
ashutosh-das <ashutoshkumar.das@saaslabs.co>
2026-08-15 23:53:18 +0530

    Time: 3 ms (33.78%), Space: 56.9 MB (11.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -2,14 +2,28 @@
  * @param {string[]} strs
  * @return {string}
  */
+/*
 var longestCommonPrefix = function(strs) {
-    strs.sort()
-    let last = strs[strs.length-1];
+   strs.sort()
+   let last = strs[strs.length-1];
+   let first = strs[0];
+   let ans='';
+   for(let i=0 ; i< last.length && i< first.length; i++){
+       if(first[i]== last[i]) ans = ans + first[i]
+       else break
+   }
+   return ans
+}; */
+var longestCommonPrefix = function (strs) {
     let first = strs[0];
-    let ans='';
-    for(let i=0 ; i< last.length && i< first.length; i++){
-        if(first[i]== last[i]) ans = ans + first[i]
-        else break
+    let ans = '';
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (!(strs[j][i] == strs[j - 1][i])) {
+                return ans;
+            }
+        }
+        ans = ans+(first[i])
     }
     return ans
 };
